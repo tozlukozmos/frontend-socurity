4e959585e315642bfb5e4b03ad53b880e2341dd6
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-02-23 15:23:06 +0300

    404NotFound page updated

--- src/views/404NotFound.vue
+++ src/views/404NotFound.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>404 NOT FOUND</h1>
+    <main class="notfound-page-main">
+        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_03wqck43.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+        <h2>Sorry... This page is not exist.</h2>
+        <button 
+        @click="$router.go(-1)" 
+        class="button-turn-back">
+        Turn Back!
+        </button>
+    </main>
 </template>
+
+<style scoped>
+.notfound-page-main{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+}
+.button-turn-back {
+    min-width: 300px;
+    margin-top: 30px;
+    font-size: 1.5rem;
+    font-weight: var(--bold);
+    color: var(--light);
+    background-color: var(--dark);
+    border: 2px solid var(--dark);
+    border-radius: 10px;
+    cursor: pointer;
+    position: relative;
+}
+.button-turn-back:focus {
+    outline: var(--dark) solid 2px;
+    outline-offset: 2px;
+}
+.button-turn-back:active {
+    outline: var(--yellow) solid 2px;
+}
+
+</style>
